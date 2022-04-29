7b1c3271ea6b02fdf98c8c41d3c21ae270ebdb3b
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-04-29 20:16:49 +0300

    Completed day 22

---
+++ Project3/Project1/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Илья Лехов on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        pictures.sort()
+        //cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = "Picture \(pictures.firstIndex(of: pictures[indexPath.row])! + 1) of \(pictures.count)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
